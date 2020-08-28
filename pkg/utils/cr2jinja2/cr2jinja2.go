bf7e1cd14b9c881dc3360d72eb1b5b033058f3e2
Howard Gao <howard.gao@gmail.com>
2020-08-28 12:08:05 +0800

    Added v3alpha1 package

--- pkg/utils/cr2jinja2/cr2jinja2.go
+++ pkg/utils/cr2jinja2/cr2jinja2.go
@@ -1,10 +1,10 @@
 package cr2jinja2
 
 import (
-	brokerv2alpha2 "github.com/artemiscloud/activemq-artemis-operator/pkg/apis/broker/v2alpha2"
+	brokerv3alpha1 "github.com/artemiscloud/activemq-artemis-operator/pkg/apis/broker/v3alpha1"
 )
 
 /* return a yaml string */
-func MakeBrokerCfgOverrides(customeResource *brokerv2alpha2.ActiveMQArtemis) string {
+func MakeBrokerCfgOverrides(customeResource *brokerv3alpha1.ActiveMQArtemis) string {
 	return "broker_home: /opt/artemis-2.14.0"
 }
